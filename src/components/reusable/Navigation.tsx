fc01141bff733027d9d22314a0eeaabe0435cdac
jspvg <josipovic1501@gmail.com>
2023-09-04 11:04:26 +0200

    Navigation - customized styling and positioning [WIP]

--- src/components/reusable/Navigation.tsx
+++ src/components/reusable/Navigation.tsx
@@ -1,62 +1,66 @@
 import logo from "../../assets/logo.svg";
-import '../../styles/styles.scss';
+import "../../styles/navigation.scss";
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img src={logo} alt="manage logo" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item centered">
-              <a className="nav-link" aria-current="page" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item centered">
-              <a className="nav-link" aria-current="page" href="#">
-                Product
-              </a>
-            </li>
-            <li className="nav-item centered">
-              <a className="nav-link" aria-current="page" href="#">
-                About Us
-              </a>
-            </li>
-            <li className="nav-item centered">
-              <a className="nav-link" aria-current="page" href="#">
-                Career
-              </a>
-            </li>
-            <li className="nav-item centered">
-              <a className="nav-link" aria-current="page" href="#">
-                Community
-              </a>
-            </li>
-            <li className="nav-item centered">
-              <a className="nav-link" aria-current="page" href="#">
-                <button className="btn btn-outline-warning" type="submit">
-                  Get Started
-                </button>
-              </a>
-            </li>
-          </ul>
+    <div className="container">
+      <nav className="navbar navbar-expand-md align-items-center">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="manage logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  Product
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  Career
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  Community
+                </a>
+              </li>
+            </ul>
+          </div>
+          <a
+            className="collapse navbar-collapse flex-grow-0 nav-link justify-content-end"
+            aria-current="page"
+            href="#"
+          >
+            <button className="btn btn-outline-warning" type="submit">
+              Get Started
+            </button>
+          </a>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
