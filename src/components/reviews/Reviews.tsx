0025d1a4ff5be1556f329f27ab7a16f261637918
jspvg <josipovic1501@gmail.com>
2023-10-10 17:25:41 +0200

    reviews - working on carousel [WIP]

--- src/components/reviews/Reviews.tsx
+++ src/components/reviews/Reviews.tsx
@@ -1,20 +1,28 @@
+import { useRef } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import GetStartedButton from '../reusable/GetStartedButton';
-import Review from './Review';
+import {ReviewComponent} from './Review';
 
 interface ReviewsProps {
   reviews: ReviewData[];
 }
 
 const Reviews: React.FC<ReviewsProps> = ({ reviews }) => {
+  const reviewRefs = useRef([]);
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
         <h1 className="fw-bold header-blue-dark text-center mb-5">
           What they've said
         </h1>
-        {reviews.map((review) => (
-          <Review key={review.id} review={review} />
-        ))}
+        <Carousel>
+          {reviews.map((review, index) => (
+            <div key={review.id} ref={(el) => (reviewRefs.current[index] = el)}>
+              <ReviewComponent review={review} ref={reviewRefs.current[index]} />
+            </div>
+          ))}
+        </Carousel>
+
         <div className="d-flex justify-content-center mt-5">
           <a href="get-started">
             <GetStartedButton />
