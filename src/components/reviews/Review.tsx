d30fc45b3aca10884c0c0cd18799549f35367591
jspvg <josipovic1501@gmail.com>
2023-10-12 14:33:18 +0200

    reviews - adding mobile-friendly styling [WIP]

--- src/components/reviews/Review.tsx
+++ src/components/reviews/Review.tsx
@@ -1,28 +1,25 @@
-import { forwardRef } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
 import { ReviewData } from '../../lib/types';
 
 interface ReviewProps {
   review: ReviewData;
 }
 
-export const ReviewComponent = forwardRef<HTMLDivElement, ReviewProps>(({ review }, ref) => {
+const Review: React.FC<ReviewProps> = ({ review }) => {
   return (
-    <Carousel.Item>
-      <div ref={ref} className="card col d-flex justify-content-center align-items-center mx-1 border-0">
-        <div className="card-body text-center p-4 bg-gray-lighter">
-          <img
-            src={review.image}
-            className="user-img mb-4"
-            alt={`${review.name}'s image`}
-          />
-          <h1 className="fw-bold header-blue-dark text-center fs-sm-6">
-            {review.name}
-          </h1>
-          <p className="text-blue-gray text-center p-2">{review.review}</p>
-        </div>
+    <div className="card row col-md d-flex justify-content-center align-items-center mx-md-1 border-0">
+      <div className="card-body m-2 m-md-0 text-center p-4 bg-gray-lighter">
+        <img
+          src={review.image}
+          className="user-img mb-4"
+          alt={`${review.name}'s image`}
+        />
+        <h1 className="fw-bold header-blue-dark text-center fs-sm-6">
+          {review.name}
+        </h1>
+        <p className="text-blue-gray text-center p-2">{review.review}</p>
       </div>
-    </Carousel.Item>
+    </div>
   );
-});
+};
 
+export default Review;
